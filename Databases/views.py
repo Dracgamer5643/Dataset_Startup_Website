dca4ca6700648dbf761813435929c1e01ad756a5
Dracgamer5643 <supermee5643@gmail.com>
2024-01-07 20:20:12 +0530

    Added Datasets pages pagaletion in views.py

--- Databases/views.py
+++ Databases/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from Home.models import pop_datasets
+from django.core.paginator import Paginator
 
 def Databases(request):
-    dbs = pop_datasets.objects.all()
-    return render(request, 'databases.html', {'dbs': dbs})
+    dbs = pop_datasets.objects.order_by('-db_rating')[:9]
+    
+    items_per_page = 9
+    paginator = Paginator(dbs, items_per_page)
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'databases.html', {'dbs': dbs, 'page_obj': page_obj})
+
